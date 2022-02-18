d49bb7775bbb40d4be8ddf4fc9ba235faccf97e6
Kirill <k261252@yandex.ru>
2022-02-18 19:27:59 +0700

    19/02

--- authdata.py
+++ authdata.py
@@ -1,3 +1,4 @@
 
 password = 'bizibro_1961'
 username = 'bizibro.ru'
+hashtag = 'surfing'
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 import random
-from authdata import username, password
+from authdata import username, password, hashtag
 #import fake_useragent from User
 
 def login(username,password):
@@ -35,4 +35,55 @@ def login(username,password):
         browser.close()
         browser.quit()
 
-login(username,password)
+def hashtag_search(username,password,hashtag):
+        try:
+            # options = webdriver.FirefoxOptions()
+            browser = webdriver.Firefox()
+            browser.get('https://instagram.com')
+            time.sleep(random.randrange(3, 5))
+            username_input = browser.find_element(By.NAME, "username")
+            username_input.clear()
+            username_input.send_keys(username)
+            time.sleep(2)
+            # пароль
+            password_input = browser.find_element(By.NAME, "password")
+            password_input.clear()
+            password_input.send_keys(password)
+            password_input.send_keys(Keys.ENTER)
+            time.sleep(7)
+            try:
+                browser.get(f'https://www.instagram.com/explore/tags/{hashtag}/')
+                time.sleep(5)
+                for i in range(1,4):
+                    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                    time.sleep(random.randrange(3,5))
+
+                hrefs = browser.find_elements(By.TAG_NAME,"a")
+                # post_urls = []
+                # for item in hrefs:
+                #     href = item.get_attribute('href')
+                #     if "/p/" in href:
+                #         post_urls.append(href)
+                #     print(href)
+                post_urls = [item.get_attribute('href') for item in hrefs if "/p/" in item.get_attribute('href')]
+                for url in post_urls[0:1]:
+                    try:
+                        browser.get(url)
+                        time.sleep(6)
+                        like_button = browser.find_element(By.XPATH,'/html/body/div[1]/section/main/div/div[1]/article/div/div[2]/div/div[2]/section[1]/span[1]/button')
+                        like_button.click()
+                        time.sleep(random.randrange(80,100))
+                    except Exception as ex:
+                        print(ex)
+            except Exception as ex:
+                print(ex)
+                browser.close()
+                browser.quit()
+        except Exception as ex:
+            print(ex)
+            browser.close()
+            browser.quit()
+
+#login(username,password)
+
+hashtag_search(username,password,hashtag)
